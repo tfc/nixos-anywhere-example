af2d1313830a75b1ce54cea1eb5a19207e588b35
Jacek Galowicz <jacek@galowicz.de>
2023-04-05 08:19:21 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Example NixOS deployment via NixOS-anywhere";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, disko, ... }@attrs: {
+    nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [
+        ({modulesPath, ... }: {
+          imports = [
+            "${modulesPath}/installer/scan/not-detected.nix"
+            "${modulesPath}/profiles/qemu-guest.nix"
+            disko.nixosModules.disko
+          ];
+          disko.devices = import ./single-gpt-disk-fullsize-ext4.nix "/dev/sda";
+          boot.loader.grub = {
+            devices = [ "/dev/sda" ];
+            efiSupport = true;
+            efiInstallAsRemovable = true;
+          };
+          services.openssh.enable = true;
+
+          users.users.root.openssh.authorizedKeys.keys = [
+            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCXfQnzmqFQsUPwJm1sQSh2A7HH1YxO6OOOn1r2QR/PqwVIRu1rOzAC5IXPKmaIN770dLIJzQMqQoUr3ih/x+zweEyUqJTP0sIjA8l9lJNj0S6xVZ594ci/C6w9fR9uKRmXIk7r6usaqTF0Jdf02Al0tB0Lv4Aqi2b6VNPLO3LT162ZuRpcqSDIZzmQg+lkd0s1jWnJGdX5s7G959ouvID5xx7g/e31M/p4PJFvdEtmZ0YGTqju+STyOvX56GvQKRlRRYVFwwTyC1KUr0fJ31dM0DjZoIrfbeY+MBO6JXT23x6iU2sywqxmrDrRphu3raLI/Y2PhopO0q7DutAoolgV cardno:6444835"
+          ];
+        })
+      ];
+    };
+  };
+}
---
+++ single-gpt-disk-fullsize-ext4.nix
@@ -0,0 +1,47 @@
+diskDevice:
+
+{
+  disk.${diskDevice} = {
+    device = diskDevice;
+    type = "disk";
+    content = {
+      type = "table";
+      format = "gpt";
+      partitions = [
+        {
+          name = "boot";
+          type = "partition";
+          start = "0";
+          end = "1M";
+          part-type = "primary";
+          flags = ["bios_grub"];
+        }
+        {
+          type = "partition";
+          name = "ESP";
+          start = "1MiB";
+          end = "100MiB";
+          bootable = true;
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        }
+        {
+          name = "root";
+          type = "partition";
+          start = "100MiB";
+          end = "100%";
+          part-type = "primary";
+          bootable = true;
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        }
+      ];
+    };
+  };
+}
